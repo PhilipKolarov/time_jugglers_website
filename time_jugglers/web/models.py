82147c56f327dbc3815ff422b5ae499b17ebf665
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-05-30 13:10:29 +0300

    Update models.py

--- time_jugglers/web/models.py
+++ time_jugglers/web/models.py
@@ -2,49 +2,6 @@ from django.core import validators
 from django.db import models
 
 
-class Album (models.Model):
-    name = models.CharField(
-        max_length=100,
-        null=False,
-        blank=False,
-    )
-
-    year = models.IntegerField(
-        null=False,
-        blank=False,
-    )
-
-    tracks_count = models.IntegerField(
-        null=False,
-        blank=False,
-    )
-
-    length = models.TimeField(
-        null=False,
-        blank=False,
-    )
-
-    recorded = models.CharField(
-        null=False,
-        blank=False,
-    )
-
-    mixed = models.CharField(
-        null=True,
-        blank=True,
-    )
-
-    mastered = models.CharField(
-        null=True,
-        blank=True,
-    )
-
-    description = models.TextField(
-        null=False,
-        blank=False,
-    )
-
-
 class Product(models.Model):
     name = models.CharField(
         max_length=100,
@@ -59,6 +16,29 @@ class Product(models.Model):
 
     image = models.ImageField(
         upload_to='/time_jugglers/staticfiles/images/merch-items',
+        null=False,
+        blank=False,
+    )
+
+
+class Contact(models.Model):
+    name = models.CharField(
+        null=False,
+        blank=False,
+    )
+
+    email = models.EmailField(
+        null=False,
+        blank=False,
+    )
+
+    subject = models.CharField(
+        max_length=150,
+        null=False,
+        blank=False,
+    )
+
+    text = models.TextField(
         null=False,
         blank=False,
     )
