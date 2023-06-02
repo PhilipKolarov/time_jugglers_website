fea46ec203aeb6bd6b8a6269d058b72f1a163ba5
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-06-02 20:51:22 +0300

    Update urls.py

--- time_jugglers/web/urls.py
+++ time_jugglers/web/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from time_jugglers.web.views import index, about, events, discography, store, store_details, contact_create
+from time_jugglers.web.views import index, about, events, discography, store, contact
 
 urlpatterns = [
     path('', index, name='index'),
@@ -10,8 +10,5 @@ urlpatterns = [
         path('', store, name='store'),
         path('details/<int:pk>/', store_details, name='store details')
     ])),
-    path('contact/', include([
-        path('create/', contact_create, name='contact create'),
-        path('sent/', contact_sent, name='contact sent'),
-    ]))
+    path('contact', contact, name='contact'),
 ]
