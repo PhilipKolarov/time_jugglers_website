6957568250719fd626c17207b9a845a94f68c43f
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-06-05 16:25:35 +0300

    Update forms.py

--- time_jugglers/web/forms.py
+++ time_jugglers/web/forms.py
@@ -2,11 +2,11 @@ from django import forms
 from time_jugglers.web.models import Contact
 
 
-class EmailBaseForm(forms.ModelForm):
+class ContactBaseForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['name', 'email', 'subject', 'text']
 
 
-class EmailCreateForm(forms.ModelForm):
+class ContactCreateForm(forms.ModelForm):
     pass
